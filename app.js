f75e72b98692473e835bbdb86720e5caa44324d8
Undisputed247 <undisputedsynergy@gmail.com>
2026-08-22 02:11:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
+const app = express()
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello from cloudpipe!');
 });
 
-app.listen(port, () => {
+
   console.log(`App running on port ${port}`);
 });
